318da2ea3972fbd491bd128e079eb9fac3ffd066
luciojaimedev <avenge1997@gmail.com>
2025-06-12 20:28:52 -0300

    chore: initial build

---
+++ script.js
@@ -0,0 +1,20 @@
+// Formas de declarar variables (las variables son cajitas)
+
+var isCharlitoChad = true; // se puede redeclarar y reasignar como quieras (no usar jamas!)
+var isCharlitoChad = false;
+
+let isSunny = true; // no se puede redeclarar pero si reasignar, deberiamos usarlo cuando necesitemos reasignar (ejemplo modo nocturno) y no es necesario inicializarla
+isSunny = false;
+
+const fingerPrint = "momo"; // no se puede redeclarar ni reasignar, deberiamos usarlo siempre que podamos (si o si tiene que ser inicializada)
+
+
+
+// Tipos de datos que puede tener una variable
+
+const catName = 'Garu' // string
+let age = 28 // number
+let isRaining = false // boolean
+let gender = null // null
+let forgotToAssign // undefined
+const pint = 31457742916835353535353n // bigint
