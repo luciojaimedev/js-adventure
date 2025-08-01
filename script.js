4070ca75a1c83885fde662a08b167c60f89a1838
luciojaimedev <avenge1997@gmail.com>
2025-08-01 20:01:10 -0300

    feat: object Promise

--- script.js
+++ script.js
@@ -1744,4 +1744,49 @@ console.log(beerFilter(beers, "ibu", true));
   console.log("Fin");
 })();
 
-console.clear()
+console.clear();
+
+function squarePromise(value) {
+  if (typeof value !== "number") {
+    return Promise.reject(
+      `Error, el valor "${value}" ingresado no es un numero`
+    );
+  }
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        value,
+        result: value * value,
+      });
+    }, 0 | (Math.random() * 1000));
+  });
+}
+
+squarePromise(0)
+  .then((obj) => {
+    console.log("Inicio Promise");
+    console.log(`Promise ${obj.value}, ${obj.result}`);
+    return squarePromise(1);
+  })
+  .then((obj) => {
+    console.log(`Promise ${obj.value}, ${obj.result}`);
+    return squarePromise(2);
+  })
+  .then((obj) => {
+    console.log(`Promise ${obj.value}, ${obj.result}`);
+    return squarePromise(3);
+  })
+  .then((obj) => {
+    console.log(`Promise ${obj.value}, ${obj.result}`);
+    return squarePromise(4);
+  })
+  .then((obj) => {
+    console.log(`Promise ${obj.value}, ${obj.result}`);
+    return squarePromise(5);
+  })
+  .then((obj) => {
+    console.log(`Promise ${obj.value}, ${obj.result}`);
+    console.log("Fin Promise");
+  })
+  .catch((err) => console.error(err));
