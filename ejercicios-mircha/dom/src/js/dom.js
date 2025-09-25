d9c0683b001f86ca782411bda6af2b1434248c35
luciojaimedev <avenge1997@gmail.com>
2025-09-25 19:52:09 -0300

    feat: alarm clock & files  reorganization

---
+++ ejercicios-mircha/dom/src/js/dom.js
@@ -0,0 +1,17 @@
+export const $hamburgerDrawer = document.querySelector(".hamburger-drawer");
+export const $hamburgerBtn = document.querySelector('[data-menu="btn"]');
+export const $playClockBtn = document.querySelector(
+  '[data-id="activate-clock"]'
+);
+export const $stopClockBtn = document.querySelector(
+  '[data-id="deactivate-clock"]'
+);
+export const $playAlarmBtn = document.querySelector(
+  '[data-id="activate-alarm"]'
+);
+export const $stopAlarmBtn = document.querySelector(
+  '[data-id="deactivate-alarm"]'
+);
+export const $clock = document.querySelector('[data-id="clock"]');
+
+export const $audio = document.createElement("audio");
