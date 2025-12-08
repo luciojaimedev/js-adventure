d320c834ca5a9be0b213c4c9e78468cd64e98764
luciojaimedev <avenge1997@gmail.com>
2025-12-08 19:08:23 -0300

    feat(SPA): add search functionality and infinite scroll

--- ejercicios-mircha/spa/App.js
+++ ejercicios-mircha/spa/App.js
@@ -1,6 +1,7 @@
 import { Header } from "./app/components/Header.js";
 import { Main } from "./app/components/Main.js";
 import { Router } from "./app/components/Router.js";
+import { InfiniteScroll } from "./app/helpers/infinite_scroll.js";
 
 export function App() {
   const $root = document.getElementById("root");
@@ -11,4 +12,5 @@ export function App() {
   $root.appendChild(Main());
 
   Router();
+  InfiniteScroll();
 }
