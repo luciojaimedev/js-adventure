80618abb85bfd520bdec879f417d9e62c1be8df0
luciojaimedev <avenge1997@gmail.com>
2025-12-16 18:00:27 -0300

    feat: finish reactive todo-list

---
+++ ejercicios-mircha/reactividad/Component.js
@@ -0,0 +1,26 @@
+const Component = (function () {
+  const Constructor = function (options) {
+    this.el = options.el;
+    this.data = options.data;
+    this.template = options.template;
+  };
+
+  Constructor.prototype.render = function () {
+    const $el = d.querySelector(this.el);
+    if (!$el) return;
+    $el.innerHTML = this.template(this.data);
+  };
+  Constructor.prototype.setState = function (obj) {
+    for (let key in obj) {
+      if (this.data.hasOwnProperty(key)) {
+        this.data[key] = obj[key];
+      }
+    }
+    this.render();
+  };
+  Constructor.prototype.getState = function () {
+    return JSON.parse(JSON.stringify(this.data));
+  };
+
+  return Constructor;
+})();
